6bcfa5b9b80f3f6b2a4b254d349aca6a82997771
imb-bestrada <bstrada4@gmail.com>
2025-10-13 00:08:20 -0500

    UPD

--- src/features/modules/planes/components/PasosCotizacion.tsx
+++ src/features/modules/planes/components/PasosCotizacion.tsx
@@ -1,19 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle as faCircleSolid } from '@fortawesome/free-solid-svg-icons';
+import type { IPasosCotizacionProps } from './interfaces/IPasosCotizacionProps.interface';
 
-interface PasosCotizacionProps {
-    activeStep: number;
-}
-
-export const PasosCotizacion = ({ activeStep }: PasosCotizacionProps) => {
+export const PasosCotizacion = ({ activeStep }: IPasosCotizacionProps) => {
     return (
         <section className="sectPasosCotizacion">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
                         <ul className="listPasosCotizacion">
-                            {/* 2. Usa una condición para la clase 'active' */}
-                            <li className={activeStep === 1 ? 'item active' : 'item'}> 
+                            <li className={activeStep >= 1 ? 'item active' : 'item'}> 
                                 <span>1</span> Planes y Coberturas
                             </li>
                             <li className="separator">
@@ -22,8 +18,7 @@ export const PasosCotizacion = ({ activeStep }: PasosCotizacionProps) => {
                                 <FontAwesomeIcon icon={faCircleSolid} />
                                 <FontAwesomeIcon icon={faCircleSolid} />
                             </li>
-                            {/* 3. Usa una condición para la clase 'active' */}
-                            <li className={activeStep === 2 ? 'item active' : 'item'}> 
+                            <li className={activeStep >= 2 ? 'item active' : 'item'}> 
                                 <span>2</span> Resumen
                             </li>
                         </ul>
